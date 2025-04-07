a35ac41749d8c28cedee0d514a28a883a0c8fb6e
HunterRedou <philipp.selke@gmail.com>
2025-04-07 16:31:41 +0200

    Build the main file

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from textnode import TextNode, TextType
+
+def main():
+    
+    test = TextNode("This is some anchor text", TextType.LINK_TEXT, "https://www.boot.dev")
+    print(test)
+    
+
+
+
+
+
+if __name__ == "__main__":
+        main()
+    
+     
